fdc5a1c913d304fb2fd1a893059074c3b6406765
Kang, ChungHyeok <kch30507@gmail.com>
2026-04-29 11:30:41 +0000

    아직 아무것도 못했다

---
+++ RSA.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <gmp.h>
+
+int main() {
+    //N을 구한다. L을 구한다. E를 구한다. D를 구한다.
+    char plaintext;
+    mpz_t p, q;
+    mpz_init_set_str(p, "12378123721083", 10);
+    while (1)
+    {
+        
+    }
+    
+    
+    return 0;
+}
